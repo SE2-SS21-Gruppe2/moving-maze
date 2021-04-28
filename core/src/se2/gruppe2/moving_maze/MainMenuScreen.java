19be7df8aee080cd3a93c8acd542ca163f4afb1c
Andreas Kogler <andy.kogler.junior@gmail.com>
2021-04-28 13:33:23 +0200

    Display background-image, logo and implement custom method for scaling pixmaps to fit width.

--- core/src/se2/gruppe2/moving_maze/MainMenuScreen.java
+++ core/src/se2/gruppe2/moving_maze/MainMenuScreen.java
@@ -1,7 +1,17 @@
 package se2.gruppe2.moving_maze;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class MainMenuScreen implements Screen {
@@ -9,9 +19,54 @@ public class MainMenuScreen implements Screen {
     final MovingMazeGame game;
     OrthographicCamera camera;
 
+    // UI stuff
+    Stage stage;
+    Skin skin;
+    Group buttonGroup;
+    TextButton createSession, joinSession, options, rules, devMode;
+    Texture headerLogoScaled;
+
+
+    // textures and views
+    Texture bgImageTexture;
+    TextureRegion bgTextureRegion;
+
     public MainMenuScreen(final MovingMazeGame game) {
         this.game = game;
         camera = MovingMazeGame.getStandardizedCamera();
+
+        // ui
+        /*
+        stage = new Stage();
+        // TODO: add path
+        skin = new Skin(Gdx.files.internal(""));
+        Gdx.input.setInputProcessor(stage);
+        buttonGroup = new Group();
+        buttonGroup.setBounds(0,0,MovingMazeGame.WIDTH, MovingMazeGame.HEIGHT);
+
+        createSession = new TextButton("Create Session", skin);
+        joinSession = new TextButton("Join Session", skin);
+        options = new TextButton("Options", skin);
+        rules = new TextButton("Rules", skin);
+        devMode = new TextButton("Developer Mode", skin);
+
+        buttonGroup.addActor(createSession);
+        buttonGroup.addActor(joinSession);
+        buttonGroup.addActor(options);
+        buttonGroup.addActor(rules);
+        buttonGroup.addActor(devMode);
+
+        stage.addActor(buttonGroup);
+        stage.getCamera().position.set(MovingMazeGame.WIDTH/2.0f, MovingMazeGame.HEIGHT/2.0f, 0);
+         */
+
+        headerLogoScaled = getScaledImage("ui/logo.png", 0.7f);
+
+
+        // instantiate textures
+        bgImageTexture = new Texture(Gdx.files.internal("ui/bg_moss.jpeg"));
+        bgTextureRegion = new TextureRegion(bgImageTexture);
+
     }
 
     @Override
@@ -25,7 +80,9 @@ public class MainMenuScreen implements Screen {
         game.batch.setProjectionMatrix(camera.combined);
 
         game.batch.begin();
-        game.font.draw(game.batch, "Main menu screen", 100, 100);
+            game.batch.draw(bgTextureRegion, 0, 0);
+            game.batch.draw(headerLogoScaled, 100, 100);
+            game.font.draw(game.batch, "Main menu screen", 100, 100);
         game.batch.end();
     }
 
@@ -52,5 +109,34 @@ public class MainMenuScreen implements Screen {
     @Override
     public void dispose() {
 
+    }
+
+    /**
+     * Scales a picture to be x percent of the current screen
+     * @param path Gdx-internal asset path to the image file
+     * @return the scaled version of the image as Texture-object
+     */
+    private Texture getScaledImage(String path, float percentOfScreen) {
+        Pixmap originalBg = new Pixmap(Gdx.files.internal(path));
+
+        // determine how much the picture has to be scaled in order to fit the screen width exactly
+        float baseScalingFactor = (originalBg.getWidth()*1.0f) / (camera.viewportWidth);
+        float scalingFactor = baseScalingFactor / percentOfScreen;
+
+        Pixmap scaledBg = new Pixmap((int) (originalBg.getWidth()/scalingFactor),
+                                    (int) (originalBg.getHeight()/scalingFactor),
+                                        originalBg.getFormat());
+
+        // scale by "redrawing" the original pixmap into the smaller pixmap
+        scaledBg.drawPixmap(originalBg,
+                0, 0, originalBg.getWidth(), originalBg.getHeight(),
+                0, 0, scaledBg.getWidth(), scaledBg.getHeight());
+
+        Texture scaledBgTexture = new Texture(scaledBg);
+
+        originalBg.dispose();
+        scaledBg.dispose();
+
+        return scaledBgTexture;
     }
 }
