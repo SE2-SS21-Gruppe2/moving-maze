4fb0162eb06dc568a90b2076db3bf7e470fcfd51
baciiu <albaciu@edu.aau.at>
2021-04-29 11:22:01 +0300

    set the Background Image in OptionScreen

--- core/src/se2/gruppe2/moving_maze/screens/OptionScreen.java
+++ core/src/se2/gruppe2/moving_maze/screens/OptionScreen.java
@@ -1,7 +1,10 @@
 package se2.gruppe2.moving_maze.screens;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.ScreenUtils;
 import se2.gruppe2.moving_maze.MovingMazeGame;
 
@@ -10,9 +13,16 @@ public class OptionScreen implements Screen {
     final MovingMazeGame game;
     OrthographicCamera camera;
 
+    Texture bgImageTexture;
+    TextureRegion bgTextureRegion;
+
+
     public OptionScreen(final MovingMazeGame game) {
         this.game = game;
         camera = MovingMazeGame.getStandardizedCamera();
+
+        bgImageTexture = new Texture(Gdx.files.internal("ui/bg_moss.jpeg")); // background image
+        bgTextureRegion = new TextureRegion(bgImageTexture);
     }
 
     @Override
@@ -22,11 +32,15 @@ public class OptionScreen implements Screen {
 
     @Override
     public void render(float delta) {
+
         ScreenUtils.clear(0,0,0,1);
+
         game.batch.setProjectionMatrix(camera.combined);
 
         game.batch.begin();
-        game.font.draw(game.batch, "Option screen", 100, 100);
+
+        game.batch.draw(bgTextureRegion, 0, 0); // draw 2D rectangles that reference a texture.
+
         game.batch.end();
     }
 
@@ -54,4 +68,9 @@ public class OptionScreen implements Screen {
     public void dispose() {
 
     }
+
+
+
+
+
 }
