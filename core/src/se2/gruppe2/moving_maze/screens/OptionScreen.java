ffabb5adda36cd61c8f70c6506069dec6e887222
baciiu <albaciu@edu.aau.at>
2021-04-29 13:56:19 +0300

    added stage - doesnt work

--- core/src/se2/gruppe2/moving_maze/screens/OptionScreen.java
+++ core/src/se2/gruppe2/moving_maze/screens/OptionScreen.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Group;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
@@ -16,19 +17,19 @@ import se2.gruppe2.moving_maze.MovingMazeGame;
 public class OptionScreen implements Screen {
 
     final MovingMazeGame game;
-    OrthographicCamera camera;
+    public OrthographicCamera camera;
 
-    Texture bgImageTexture;
-    TextureRegion bgTextureRegion;
+    public Texture bgImageTexture;
+    public TextureRegion bgTextureRegion;
 
-    Label soundLabel, difficultyLabel, modeLabel; // dummy labels
-    Button soundButton,difficultyButton,modeButton; // dummy buttons
+    public Label soundLabel, difficultyLabel, modeLabel; // dummy labels
+    public Button soundButton,difficultyButton,modeButton; // dummy buttons
 
-    Group labelGroup ;
-    Group buttonGroup;
+    public Group labelGroup ;
+    public Group buttonGroup;
 
-    Skin skin;
-
+    public Skin skin;
+    public Stage stage;
 
     public OptionScreen(final MovingMazeGame game) {
 
@@ -36,6 +37,9 @@ public class OptionScreen implements Screen {
         camera = MovingMazeGame.getStandardizedCamera();
 
         skin = new Skin(Gdx.files.internal("ui/uiskin.json"));
+
+        this.stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
 
         buttonGroup = new Group();
         labelGroup = new Group();
@@ -63,6 +67,10 @@ public class OptionScreen implements Screen {
         labelGroup.addActor(difficultyLabel);
         labelGroup.addActor(modeLabel);
 
+        stage.addActor(labelGroup);
+        stage.addActor(buttonGroup);
+
+        stage.getCamera().position.set(MovingMazeGame.WIDTH/2.0f, MovingMazeGame.HEIGHT/2.0f, 0);
 
         bgImageTexture = new Texture(Gdx.files.internal("ui/bg_moss.jpeg")); // background image
         bgTextureRegion = new TextureRegion(bgImageTexture);
@@ -77,13 +85,10 @@ public class OptionScreen implements Screen {
     public void render(float delta) {
 
         ScreenUtils.clear(0,0,0,1);
-
         game.batch.setProjectionMatrix(camera.combined);
-
         game.batch.begin();
-
         game.batch.draw(bgTextureRegion, 0, 0); // draw 2D rectangles that reference a texture.
-
+        stage.draw();
         game.batch.end();
     }
 
@@ -111,7 +116,6 @@ public class OptionScreen implements Screen {
     public void dispose() {
 
     }
-
 
     /**
      * Initializes the positions of all buttons.
