69a5cc98a74b744d833780a76203d316b9f63819
baciiu <albaciu@edu.aau.at>
2021-04-30 15:37:08 +0300

    added title and an image for sound

--- core/src/se2/gruppe2/moving_maze/screens/OptionScreen.java
+++ core/src/se2/gruppe2/moving_maze/screens/OptionScreen.java
@@ -6,20 +6,25 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.Drawable;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.ScreenUtils;
 import se2.gruppe2.moving_maze.MovingMazeGame;
 
 public class OptionScreen implements Screen {
 
-    static float volume = 1.0f;
     final MovingMazeGame game;
     public OrthographicCamera camera;
 
     public Texture bgImageTexture;
     public TextureRegion bgTextureRegion;
+
+    public Label title;
 
     public Label soundLabel, difficultyLabel, modeLabel; // dummy labels
     public Button soundButton,difficultyButton,modeButton; // dummy buttons
@@ -27,8 +32,12 @@ public class OptionScreen implements Screen {
     public Skin skin;
     public Stage stage;
 
-    private Table table1,table2;
+    private Table table1;
 
+    private int music = 1 ;
+
+    Drawable sound_on_drawable = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("ui/sound_on.png"))));
+    Drawable sound_off_drawable = new TextureRegionDrawable(new TextureRegion(new Texture(Gdx.files.internal("ui/sound_off.png"))));
 
     public OptionScreen(final MovingMazeGame game) {
 
@@ -44,13 +53,17 @@ public class OptionScreen implements Screen {
 
         Gdx.input.setInputProcessor(stage);
 
+        title = new Label("OPTIONS",skin);
+
+        setTitleCoordinates();
+
         table1 = new Table();
 
         soundLabel = new Label("Sound",skin);
         difficultyLabel = new Label("Difficulty",skin);
         modeLabel = new Label("Mode", skin);
 
-        soundButton = new TextButton("ON",skin);
+        soundButton = new ImageButton(sound_on_drawable);
         difficultyButton = new TextButton("easy",skin);
         modeButton = new TextButton("single",skin);
 
@@ -58,6 +71,7 @@ public class OptionScreen implements Screen {
         setTable1();
         setUpButtonListeners();
 
+        stage.addActor(title);
         stage.addActor(table1);
 
         stage.getCamera().position.set(MovingMazeGame.WIDTH/2.0f, MovingMazeGame.HEIGHT/2.0f, 0);
@@ -101,12 +115,12 @@ public class OptionScreen implements Screen {
 
     }
 
+    private void setTitleCoordinates(){
 
-     /* Initializes the positions of all buttons */
-
-    private void initLabelPositions() {
-
-
+        title.setAlignment(Align.center);
+        title.setSize(20f,10f);
+        title.setX( camera.viewportWidth/2 - title.getWidth()/2.0f );
+        title.setY(camera.viewportHeight - title.getHeight()-30);
     }
 
     private void setTable1(){
@@ -120,13 +134,16 @@ public class OptionScreen implements Screen {
     private void initTable1(){
 
         table1.align(Align.center);
-        table1.add(soundLabel).size(200,70).center();
-        table1.add(soundButton).size(50,30).center();
+
+        table1.add(soundLabel).size(170,70).center();
+        table1.add(soundButton).size(60,60).center();
+
         table1.row();
-        table1.add(difficultyLabel).size(200,70).center();
+        table1.add(difficultyLabel).size(170,70).center();
         table1.add(difficultyButton).size(50,30).center();
+
         table1.row();
-        table1.add(modeLabel).size(200,70).center();
+        table1.add(modeLabel).size(170,70).center();
         table1.add(modeButton).size(60,30).center();
 
         table1.padTop(180);
@@ -135,6 +152,24 @@ public class OptionScreen implements Screen {
     }
 
     private void setUpButtonListeners(){
+
+        soundButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+
+                if (soundButton.isPressed()){
+
+                    if (music == 1 ){
+                        music = 0 ;
+                    }else {
+                        music = 1;
+                    }
+                    System.out.println("Music is " + music );
+                }
+
+
+            }
+        });
     }
 
 
