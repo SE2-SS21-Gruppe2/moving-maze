b935908e7ea13b4e5649b3321140505f1314e278
baciiu <albaciu@edu.aau.at>
2021-04-30 12:12:57 +0300

    labels and buttons are visible

--- core/src/se2/gruppe2/moving_maze/screens/OptionScreen.java
+++ core/src/se2/gruppe2/moving_maze/screens/OptionScreen.java
@@ -1,3 +1,4 @@
+
 package se2.gruppe2.moving_maze.screens;
 
 import com.badlogic.gdx.Gdx;
@@ -5,17 +6,15 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Button;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.utils.Align;
 import com.badlogic.gdx.utils.ScreenUtils;
 import se2.gruppe2.moving_maze.MovingMazeGame;
 
 public class OptionScreen implements Screen {
 
+    static float volume = 1.0f;
     final MovingMazeGame game;
     public OrthographicCamera camera;
 
@@ -25,13 +24,14 @@ public class OptionScreen implements Screen {
     public Label soundLabel, difficultyLabel, modeLabel; // dummy labels
     public Button soundButton,difficultyButton,modeButton; // dummy buttons
 
-    public Group labelGroup ;
-    public Group buttonGroup;
-
     public Skin skin;
     public Stage stage;
 
+    private Table table1,table2;
+
+
     public OptionScreen(final MovingMazeGame game) {
+
         this.game = game;
     }
 
@@ -39,45 +39,31 @@ public class OptionScreen implements Screen {
     public void show() {
         camera = MovingMazeGame.getStandardizedCamera();
 
+        this.stage = new Stage();
         skin = new Skin(Gdx.files.internal("ui/uiskin.json"));
 
-        this.stage = new Stage();
         Gdx.input.setInputProcessor(stage);
 
-        buttonGroup = new Group();
-        labelGroup = new Group();
-
-        buttonGroup.setBounds(0,0,MovingMazeGame.WIDTH, MovingMazeGame.HEIGHT);
-        labelGroup.setBounds(0,0,MovingMazeGame.WIDTH, MovingMazeGame.HEIGHT);
+        table1 = new Table();
 
         soundLabel = new Label("Sound",skin);
-        difficultyLabel = new Label("Difficulty", skin);
+        difficultyLabel = new Label("Difficulty",skin);
         modeLabel = new Label("Mode", skin);
 
         soundButton = new TextButton("ON",skin);
         difficultyButton = new TextButton("easy",skin);
         modeButton = new TextButton("single",skin);
 
-        initButtonPositions();
-        initLabelPositions();
+        initTable1();
+        setTable1();
         setUpButtonListeners();
 
-        buttonGroup.addActor(soundButton);
-        buttonGroup.addActor(difficultyButton);
-        buttonGroup.addActor(modeButton);
-
-        labelGroup.addActor(soundLabel);
-        labelGroup.addActor(difficultyLabel);
-        labelGroup.addActor(modeLabel);
-
-        stage.addActor(labelGroup);
-        stage.addActor(buttonGroup);
+        stage.addActor(table1);
 
         stage.getCamera().position.set(MovingMazeGame.WIDTH/2.0f, MovingMazeGame.HEIGHT/2.0f, 0);
 
         bgImageTexture = new Texture(Gdx.files.internal("ui/bg_moss.jpeg")); // background image
-        bgTextureRegion = new TextureRegion(bgImageTexture);
-    }
+        bgTextureRegion = new TextureRegion(bgImageTexture);}
 
     @Override
     public void render(float delta) {
@@ -85,9 +71,9 @@ public class OptionScreen implements Screen {
         ScreenUtils.clear(0,0,0,1);
         game.batch.setProjectionMatrix(camera.combined);
         game.batch.begin();
-        game.batch.draw(bgTextureRegion, 0, 0); // draw 2D rectangles that reference a texture.
-        stage.draw();
+        game.batch.draw(bgTextureRegion, 0, 0);
         game.batch.end();
+        stage.draw();
     }
 
     @Override
@@ -115,32 +101,44 @@ public class OptionScreen implements Screen {
 
     }
 
-    /**
-     * Initializes the positions of all buttons.
-     */
+
+     /* Initializes the positions of all buttons */
+
     private void initLabelPositions() {
-       /*
-        soundLabel.setPosition(x,y);
-        difficultyLabel.setPosition(x,y);
-        modeLabel.setPosition(x,y);
-        */
+
 
     }
 
-    /**
-     * Initializes the positions of all buttons.
-     */
-    private void initButtonPositions() {
-        /*
-        soundButton.setPosition(x,y);
-        difficultyButton.setPosition(x,y);
-        modeButton.setPosition(x,y);
-        */
+    private void setTable1(){
+
+        table1.align(Align.center);
+        table1.setPosition(MovingMazeGame.WIDTH/2.5f,MovingMazeGame.HEIGHT/2.5f,Align.center);
+        table1.padBottom(290);
+    }
+
+
+    private void initTable1(){
+
+        table1.align(Align.center);
+        table1.add(soundLabel).size(200,70).center();
+        table1.add(soundButton).size(50,30).center();
+        table1.row();
+        table1.add(difficultyLabel).size(200,70).center();
+        table1.add(difficultyButton).size(50,30).center();
+        table1.row();
+        table1.add(modeLabel).size(200,70).center();
+        table1.add(modeButton).size(60,30).center();
+
+        table1.padTop(180);
+        table1.padLeft(130);
 
     }
 
     private void setUpButtonListeners(){
-
     }
 
+
+
+
 }
+
