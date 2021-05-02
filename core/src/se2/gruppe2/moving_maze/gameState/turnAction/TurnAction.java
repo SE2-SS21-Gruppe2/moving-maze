faf2cb436b5ef273ee4aac4b2cf81cf94a4ed194
NicoOtt1998 <nico.ott1998@gmail.com>
2021-05-02 16:20:20 +0200

    the remaining class varibals and funktions headers implementet

--- core/src/se2/gruppe2/moving_maze/gameState/turnAction/TurnAction.java
+++ core/src/se2/gruppe2/moving_maze/gameState/turnAction/TurnAction.java
@@ -1,4 +1,7 @@
 package se2.gruppe2.moving_maze.gameState.turnAction;
 
 public interface TurnAction {
+    void execute();
+    boolean validate();
+
 }
--- core/src/se2/gruppe2/moving_maze/player/Player.java
+++ core/src/se2/gruppe2/moving_maze/player/Player.java
@@ -1,4 +1,17 @@
 package se2.gruppe2.moving_maze.player;
 
+import se2.gruppe2.moving_maze.item.Card;
+import se2.gruppe2.moving_maze.item.Position;
+
 public class Player {
+    private String name;
+    private Position pos;
+    private Card[] cardsToFind;
+    private Card[] cardsFound;
+    private Card[] currentCard;
+    private PlayerRole role;
+
+    public Position move(int dX, int dy){
+        return new Position();
+    }
 }
