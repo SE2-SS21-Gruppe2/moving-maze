d89081fb37d1de9afc9ef2d3e2c7a0990d94b724
baciiu <albaciu@edu.aau.at>
2021-05-08 17:40:43 +0300

    Added Gold Margin for the LoadingBar

--- core/src/se2/gruppe2/moving_maze/screens/LoadingScreen.java
+++ core/src/se2/gruppe2/moving_maze/screens/LoadingScreen.java
@@ -5,9 +5,12 @@ import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.scenes.scene2d.Stage;
 import se2.gruppe2.moving_maze.MovingMazeGame;
 
 public class LoadingScreen implements Screen {
@@ -15,11 +18,19 @@ public class LoadingScreen implements Screen {
     private final MovingMazeGame game;
     private ShapeRenderer shapeRenderer;
     private float progress;
+    private Texture loadingBar;
+    SpriteBatch batch;
+    private Stage stage;
+
 
 
     public LoadingScreen(MovingMazeGame game){
         this.game = game;
         this.shapeRenderer = new ShapeRenderer();
+        loadingBar = new Texture(Gdx.files.internal("ui/loadingBar.png"));
+        stage = new Stage();
+        batch = new SpriteBatch();
+
     }
 
     private void queueAssets() {
@@ -53,13 +64,24 @@ public class LoadingScreen implements Screen {
         update(delta);
 
 
+
+
+
         shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
         shapeRenderer.setColor(Color.BLACK);
-        shapeRenderer.rect(32, game.camera.viewportHeight / 2 - 8, game.camera.viewportWidth - 64, 16);
+        shapeRenderer.rect(game.camera.viewportWidth/4, game.camera.viewportHeight / 2 , game.camera.viewportWidth - 64, 16);
 
-        shapeRenderer.setColor(Color.BLUE);
-        shapeRenderer.rect(32, game.camera.viewportHeight / 2 - 8, progress * (game.camera.viewportWidth - 64), 16);
+
+
+        shapeRenderer.setColor(Color.TEAL);
+        shapeRenderer.rect(game.camera.viewportWidth/4, game.camera.viewportHeight / 2 , progress * (game.camera.viewportWidth/2f - 25), 16);
         shapeRenderer.end();
+
+        game.batch.begin();
+        game.batch.draw(new TextureRegion(loadingBar), game.camera.viewportWidth/4 - 124 , game.camera.viewportHeight / 2 - 75 );
+        game.batch.end();
+
+
 
     }
 
