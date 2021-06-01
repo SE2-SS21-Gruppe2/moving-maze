9af9b95047156cf711de6e5b041309fe97706a16
Martin Köfer <makoefer@edu.aau.at>
2021-06-01 10:03:29 +0200

    player gets moved when tile is inserted

--- core/src/main/java/se_ii/gruppe2/moving_maze/gamestate/turnAction/InsertTile.java
+++ core/src/main/java/se_ii/gruppe2/moving_maze/gamestate/turnAction/InsertTile.java
@@ -4,6 +4,9 @@ package se_ii.gruppe2.moving_maze.gamestate.turnAction;
 import com.badlogic.gdx.math.Vector2;
 import se_ii.gruppe2.moving_maze.MovingMazeGame;
 import se_ii.gruppe2.moving_maze.gameboard.GameBoard;
+import se_ii.gruppe2.moving_maze.player.Player;
+
+import java.util.List;
 
 public class InsertTile implements TurnAction {
 
@@ -27,40 +30,54 @@ public class InsertTile implements TurnAction {
         newGameBoard.setExtraTile(gameBoard.getExtraTile());
         newGameBoard.setBoard(gameBoard.getBoard());
 
+        List<Player> playersOnTile = null;
+
         int boardLength = game.getGameState().getBoard().getBoard().length;
 
         if (insertPosition.x == 0){
             var newExtraTile = newGameBoard.getBoard()[(int)insertPosition.y][boardLength-1];
+            movePlayers(boardLength-1, (int)insertPosition.y, -1, -1);
             for (var i = boardLength-1; i > 0; i--){
                 newGameBoard.getBoard()[(int)insertPosition.y][i] = newGameBoard.getBoard()[(int)insertPosition.y][i-1];
+                movePlayers(i-1, (int)insertPosition.y, i, (int)insertPosition.y);
             }
+            movePlayers(-1,-1, 0, (int)insertPosition.y);
             newGameBoard.getBoard()[(int)insertPosition.y][0] = newGameBoard.getExtraTile();
             newGameBoard.setExtraTile(newExtraTile);
             lastInsertPosition = new Vector2(6, insertPosition.y);
         }
         else if (insertPosition.x == 6){
             var newExtraTile = newGameBoard.getBoard()[(int)insertPosition.y][0];
+            movePlayers(0, (int)insertPosition.y, -1, -1);
             for (var i = 0; i < boardLength-1; i++){
                 newGameBoard.getBoard()[(int)insertPosition.y][i] = newGameBoard.getBoard()[(int)insertPosition.y][i+1];
+                movePlayers(i+1, (int)insertPosition.y, i, (int)insertPosition.y);
             }
+            movePlayers(-1, -1, boardLength-1, (int)insertPosition.y);
             newGameBoard.getBoard()[(int)insertPosition.y][boardLength-1] = newGameBoard.getExtraTile();
             newGameBoard.setExtraTile(newExtraTile);
             lastInsertPosition = new Vector2(0, insertPosition.y);
         }
         else if (insertPosition.y == 0){
             var newExtraTile = newGameBoard.getBoard()[boardLength-1][(int)insertPosition.x];
+            movePlayers((int)insertPosition.x, boardLength-1, -1, -1);
             for (var i = boardLength-1; i > 0; i--){
                 newGameBoard.getBoard()[i][(int)insertPosition.x] = newGameBoard.getBoard()[i-1][(int)insertPosition.x];
+                movePlayers((int)insertPosition.x,i-1, (int)insertPosition.x, i);
             }
+            movePlayers(-1, -1, (int)insertPosition.x, 0);
             newGameBoard.getBoard()[0][(int)insertPosition.x] = newGameBoard.getExtraTile();
             newGameBoard.setExtraTile(newExtraTile);
             lastInsertPosition = new Vector2(insertPosition.x, 6);
         }
         else if (insertPosition.y == 6){
             var newExtraTile = newGameBoard.getBoard()[0][(int)insertPosition.x];
+            movePlayers((int)insertPosition.x, 0, -1, -1);
             for (var i = 0; i < boardLength-1; i++){
                 newGameBoard.getBoard()[i][(int)insertPosition.x] = newGameBoard.getBoard()[i+1][(int)insertPosition.x];
+                movePlayers((int)insertPosition.x,i+1, (int)insertPosition.x, i);
             }
+            movePlayers(-1, -1, (int)insertPosition.x, boardLength-1);
             newGameBoard.getBoard()[boardLength-1][(int)insertPosition.x] = newGameBoard.getExtraTile();
             newGameBoard.setExtraTile(newExtraTile);
             lastInsertPosition = new Vector2(insertPosition.x, 0);
@@ -91,4 +108,18 @@ public class InsertTile implements TurnAction {
             return false;
         }
     }
+
+
+    private void movePlayers(int oldY, int oldX, int newX, int newY){
+        List<Player> playersOnTile = game.getGameState().playersOnTile(oldX, oldY);
+
+        if (playersOnTile.size() != 0){
+            for (Player p : playersOnTile){
+                Player p1 = game.getGameState().getPlayerByName(p.getName());
+                p1.setPos(newX, newY);
+            }
+        }
+
+    }
+
 }
--- core/src/main/java/se_ii/gruppe2/moving_maze/player/Player.java
+++ core/src/main/java/se_ii/gruppe2/moving_maze/player/Player.java
@@ -1,5 +1,6 @@
 package se_ii.gruppe2.moving_maze.player;
 
+import com.badlogic.gdx.math.Vector2;
 import se_ii.gruppe2.moving_maze.item.Card;
 import se_ii.gruppe2.moving_maze.item.ItemLogical;
 import se_ii.gruppe2.moving_maze.item.Position;
@@ -37,6 +38,7 @@ public class Player {
                 this.pos.getY()+dY
         );
     }
+
 
     public static void getTexturePathByColor(PlayerColor color) {
 
@@ -102,6 +104,8 @@ public class Player {
     public void setPos(Position pos) {
         this.pos = pos;
     }
+
+    public void setPos(int x, int y) { this.pos.setPosition(x, y);}
 
     public Position getPos() {
         return this.pos;
