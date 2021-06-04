11d9235bcdacaba0ef719a7ddf0a41f06836158b
Florian Lanz <florianlanz73@gmail.com>
2021-06-04 14:11:46 +0200

    made the Optionsscreen bigger, that it can be read. Changed the button to the Imagebutton.

--- core/src/main/java/se_ii/gruppe2/moving_maze/screens/OptionScreen.java
+++ core/src/main/java/se_ii/gruppe2/moving_maze/screens/OptionScreen.java
@@ -35,9 +35,12 @@ public class OptionScreen implements Screen {
     private Button vibrationButton;
     private Button rotateScreenButton;
 
-    private Button backButton;
+    private Texture backTexture;
+    private TextureRegion textureRegion;
+    private TextureRegionDrawable textureRegionDrawable;
+    private ImageButton backButton;
 
-    private Skin skin ;
+    private Skin skin;
     private Stage stage;
 
     private Table table1;
@@ -68,22 +71,30 @@ public class OptionScreen implements Screen {
 
         Gdx.input.setInputProcessor(stage);
 
-        title = new Label("OPTIONS",skin);
+        title = new Label("OPTIONS", skin);
 
         setTitleCoordinates();
 
         table1 = new Table();
 
-        soundLabel = new Label("Sound",skin);
-        vibrationLabel = new Label("Vibration",skin);
-        rotateScreenLabel = new Label("Rotate the Screen", skin);
+        soundLabel = new Label("Sound", skin);
+        soundLabel.setFontScale(3f);
+        vibrationLabel = new Label("Vibration", skin);
+        vibrationLabel.setFontScale(3f);
+        rotateScreenLabel = new Label("Tile control", skin);
+        rotateScreenLabel.setFontScale(3f);
 
-        backButton = new TextButton("BACK",skin);
-        backButton.setPosition( camera.viewportWidth/4f, camera.viewportHeight - title.getHeight()-80f, Align.left);
+        //Button
+        backTexture = new Texture(Gdx.files.internal("ui/buttons/backButton.png"));
+        textureRegion = new TextureRegion(backTexture);
+        textureRegionDrawable = new TextureRegionDrawable(textureRegion);
+        backButton = new ImageButton(textureRegionDrawable);
+        backButton.setPosition(20f, camera.viewportHeight - 100f, Align.left);
 
-        soundButton = new ImageButton(soundOnDrawable,soundOffDrawable,soundOffDrawable);
-        vibrationButton = new ImageButton(vibrateOnDrawable,vibrateOffDrawable,vibrateOffDrawable);
-        rotateScreenButton = new ImageButton(rotateOnDrawable,rotateOffDrawable,rotateOffDrawable);
+
+        soundButton = new ImageButton(soundOnDrawable, soundOffDrawable, soundOffDrawable);
+        vibrationButton = new ImageButton(vibrateOnDrawable, vibrateOffDrawable, vibrateOffDrawable);
+        rotateScreenButton = new ImageButton(rotateOnDrawable, rotateOffDrawable, rotateOffDrawable);
 
         soundButton.setChecked(true);
 
@@ -95,7 +106,7 @@ public class OptionScreen implements Screen {
         stage.addActor(table1);
 
         stage.addActor(backButton);
-        stage.getCamera().position.set(MovingMazeGame.WIDTH/2.0f, MovingMazeGame.HEIGHT/2.0f, 0);
+        stage.getCamera().position.set(MovingMazeGame.WIDTH / 2.0f, MovingMazeGame.HEIGHT / 2.0f, 0);
 
         bgImageTexture = new Texture(Gdx.files.internal("ui/bg_moss.jpeg")); // background image
         bgTextureRegion = new TextureRegion(bgImageTexture);
@@ -104,12 +115,10 @@ public class OptionScreen implements Screen {
     }
 
 
-
-
     @Override
     public void render(float delta) {
 
-        ScreenUtils.clear(0,0,0,1);
+        ScreenUtils.clear(0, 0, 0, 1);
         batch.setProjectionMatrix(camera.combined);
         batch.begin();
         batch.draw(bgTextureRegion, 0, 0);
@@ -142,43 +151,43 @@ public class OptionScreen implements Screen {
         // lifecycle function
     }
 
-    private void setTitleCoordinates(){
+    private void setTitleCoordinates() {
 
         title.setAlignment(Align.center);
-        title.setSize(20f,10f);
-        title.setX( camera.viewportWidth/2f - title.getWidth()/2f );
-        title.setY(camera.viewportHeight - title.getHeight()-80f);
+        title.setFontScale(4f);
+        title.setX(camera.viewportWidth / 2f - title.getWidth() / 2f);
+        title.setY(camera.viewportHeight - title.getHeight() - 80f);
     }
 
-    private void setTable1(){
+    private void setTable1() {
 
-        table1.align(Align.center);
-        table1.setPosition(MovingMazeGame.WIDTH/2.5f,MovingMazeGame.HEIGHT/2.5f,Align.center);
+        table1.setPosition(Gdx.graphics.getWidth() / 2f - table1.getWidth() / 2f,
+                Gdx.graphics.getHeight() / 2f + table1.getHeight() / 2f, Align.center);
         table1.padBottom(290);
     }
 
 
-    private void initTable1(){
+    private void initTable1() {
 
         table1.align(Align.center);
 
-        table1.add(soundLabel).size(170,70).center();
-        table1.add(soundButton).size(60,60).center();
-
+        table1.add(soundLabel).size(300, 100).center();
+        table1.add(soundButton).size(100, 100).center();
         table1.row();
-        table1.add(vibrationLabel).size(170,70).center();
-        table1.add(vibrationButton).size(60,60).center();
 
+        table1.add(vibrationLabel).size(300, 100).center();
+        table1.add(vibrationButton).size(100, 100).center();
         table1.row();
-        table1.add(rotateScreenLabel).size(170,70).center();
-        table1.add(rotateScreenButton).size(50,50).center();
 
-        table1.padTop(180);
-        table1.padLeft(130);
+        table1.add(rotateScreenLabel).size(300, 100).center();
+        table1.add(rotateScreenButton).size(100, 100).center();
+
+        table1.pad(150);
+        //table1.padLeft(130);
 
     }
 
-    private void setUpButtonListeners(){
+    private void setUpButtonListeners() {
 
         soundButton.addListener(new ClickListener() {
             @Override
@@ -187,12 +196,12 @@ public class OptionScreen implements Screen {
 
                 musicInc++;
 
-                if (musicInc % 2 == 0){
+                if (musicInc % 2 == 0) {
 
                     audioManager.getBackgroundMusic().play(0.4f);
                     audioManager.getBackgroundMusic().loop();
 
-                }else {
+                } else {
 
                     audioManager.getBackgroundMusic().stop();
 
