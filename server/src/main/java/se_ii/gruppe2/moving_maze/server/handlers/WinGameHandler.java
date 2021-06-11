85446ec9ab888c92563d02c4564ac47c91133b4b
NicoOtt1998 <nico.ott1998@gmail.com>
2021-06-11 15:19:39 +0200

    Sends Victory Messeage to all Connected Player

--- server/src/main/java/se_ii/gruppe2/moving_maze/server/handlers/WinGameHandler.java
+++ server/src/main/java/se_ii/gruppe2/moving_maze/server/handlers/WinGameHandler.java
@@ -4,8 +4,15 @@ import com.esotericsoftware.kryonet.Connection;
 import com.esotericsoftware.kryonet.Listener;
 import com.esotericsoftware.minlog.Log;
 
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.swing.text.html.HTMLDocument;
+
 import se_ii.gruppe2.moving_maze.network.messages.in.WinGameConformation;
 import se_ii.gruppe2.moving_maze.network.messages.out.WinGameRequest;
+import se_ii.gruppe2.moving_maze.player.Player;
+import se_ii.gruppe2.moving_maze.server.Session;
 import se_ii.gruppe2.moving_maze.server.SessionManager;
 
 public class WinGameHandler extends Listener {
@@ -16,7 +23,12 @@ public class WinGameHandler extends Listener {
             WinGameRequest win= (WinGameRequest) obj;
             if(true/*win.getPlayer().getCardsToFind().size()==0*/){
                 WinGameConformation winConf= new WinGameConformation(win.getPlayer());
-                con.sendTCP(winConf);
+                var session= SessionManager.getSessionByKey(win.getSessionCode());
+                Connection connection;
+                for(Map.Entry<Player,Connection> entry: session.getPlayers().entrySet()){
+                    connection= entry.getValue();
+                    connection.sendTCP(winConf);
+                }
                 SessionManager.closeSession(win.getSessionCode());
             }
 
